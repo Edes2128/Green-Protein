2a00e068983720251db26c468317532001f56b3c
Edes Sulce <edessulce@outlook.com>
2020-10-15 16:13:21 +0200

    test

--- src/App.js
+++ src/App.js
@@ -12,8 +12,111 @@ function App() {
       style={{ backgroundColor: "#121212" }}
     >
       <NavBar />
-      <Container maxWidth="lg"  style={{height:'800px',boxShadow:'0 2px 5px rgba(255,255,255,.1)',marginTop:'50px'}}>
-              
+      <Container
+        maxWidth="lg"
+        style={{
+          marginTop: "75px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/1.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/2.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/3.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/4.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/5.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/6.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/7.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/8.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/9.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/10.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/11.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/12.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/13.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/14.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/15.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/16.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/17.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/18.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/19.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/20.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/21.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/22.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/23.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/24.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/25.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/26.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/27.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/28.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/29.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/30.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/31.jpg" alt="GreenMenu" />
+        </Box>
+        <Box width="65%" mb="40px">
+          <img width="100%" src="./images/al/32.jpg" alt="GreenMenu" />
+        </Box>
       </Container>
     </Box>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 
 export default function NavBar() {
   return (
     <div>
-      <AppBar position="static"  style={{backgroundColor:'#121212'}}>
+      <AppBar   style={{backgroundColor:'#121212',position:'fixed'}}>
         <Toolbar style={{display:'flex',justifyContent:'space-between',width:'100%'}} >
-          <img  width="200" src="/green-logo.png"/>
+          <img   src="/green-logo.png" alt="Green Logo"/>
           <IconButton color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
