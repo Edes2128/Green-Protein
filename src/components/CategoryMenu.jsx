56f91ba64ea74a675389f3a7da8bf160ab26177d
Edes Sulce <edessulce@outlook.com>
2020-10-16 22:14:43 +0200

    Miranda

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CloseIcon from '@material-ui/icons/Close';
 import { Link } from 'react-router-dom';
-import  {SiTripadvisor,SiInstagram,SiFacebook} from 'react-icons/si'
+import { SiTripadvisor, SiInstagram, SiFacebook } from 'react-icons/si'
 
 export default function CategoryMenu({ showCategory, closeCategory }) {
     return (
@@ -10,29 +10,34 @@ export default function CategoryMenu({ showCategory, closeCategory }) {
             <div className={!showCategory ? 'category-menu-container' : 'category-menu-container active-category'}>
                 <div style={{ height: '10%', width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0 10px' }}>
                     <CloseIcon style={{ fontSize: '40px', cursor: 'pointer', color: 'white' }} onClick={() => closeCategory()} />
-                    <Link to={window.location.pathname === '/en' ? '/' : '/en'} style={{fontSize:'24px', color: '#8C8A8B' ,  textDecoration:'none' }}>{window.location.pathname === '/en' ? 'AL' : 'EN'}</Link>
+                    <Link to={window.location.pathname === '/en' ? '/' : '/en'} style={{ fontSize: '24px', color: '#8C8A8B', textDecoration: 'none' }}>{window.location.pathname === '/en' ? <img width="40" src="/al.png" alt="" /> : <img width="40" src="/en.png" alt="" />}</Link>
                 </div>
                 <nav style={{ height: '70%', width: '100%', alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
-                    <ul style={{ listStyle: 'none', height: '100%', width: '60%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between',alignItems:'center' }}>
-                        <li className="caategory-li" onClick={() => closeCategory()}> <img width="50" src="/hot-soup.png" alt=""/> <a className="link-category" href="#supat">{window.location.pathname === '/en' ? 'Soups' : 'Supat'} </a></li>
-                        <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/salad.png" alt=""/><a className="link-category" href="#salad">{window.location.pathname === '/en' ? 'Salads' : 'Sallatat'}</a></li>
-                        <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/wrap.png" alt=""/><a className="link-category" href="#wrap">Wraps</a></li>
-                        <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/burger.png" alt=""/><a className="link-category" href="#burger">{window.location.pathname === '/en' ? 'Burgers' : 'Burgera'}</a></li>
-                        <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/smoothie.png" alt=""/><a className="link-category" href="#smoothie">Smoothie bowls</a></li>
-                        <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/orange-juice.png" alt=""/><a className="link-category" href="#fresh">{window.location.pathname === '/en' ? 'Fresh Juice' : 'Lengje te fresket'}</a></li>
-                        <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/snack.png" alt=""/><a className="link-category" href="#snacks">{window.location.pathname === '/en' ? 'Snacks' : 'Zemrat'}</a></li>
-                        <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/sweet-food.png" alt=""/><a className="link-category" href="#sweets">{window.location.pathname === '/en' ? 'Sweets' : 'Embelsira'}</a></li>
-                        <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/water-bottle.png" alt=""/><a className="link-category" href="#other">{window.location.pathname === '/en' ? 'Other drinks' : 'Pije te tjera'}</a></li>
+                    <ul className="category-ul">
+                        <div className="left-category">
+                            <li className="caategory-li" onClick={() => closeCategory()}> <img width="50" src="/hot-soup.png" alt="" /> <a className="link-category" href="#supat">{window.location.pathname === '/en' ? 'Soups' : 'Supat'} </a></li>
+                            <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/salad.png" alt="" /><a className="link-category" href="#salad">{window.location.pathname === '/en' ? 'Salads' : 'Sallatat'}</a></li>
+                            <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/wrap.png" alt="" /><a className="link-category" href="#wrap">Wraps</a></li>
+                            <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/burger.png" alt="" /><a className="link-category" href="#burger">{window.location.pathname === '/en' ? 'Burgers' : 'Burgera'}</a></li>
+                            <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/smoothie.png" alt="" /><a className="link-category" href="#smoothie">Smoothie bowls</a></li>
+                        </div>
+                        <div className="right-category">
+
+                            <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/orange-juice.png" alt="" /><a className="link-category" href="#fresh">{window.location.pathname === '/en' ? 'Fresh Juice' : 'Lengje te fresket'}</a></li>
+                            <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/snack.png" alt="" /><a className="link-category" href="#snacks">{window.location.pathname === '/en' ? 'Snacks' : 'Zemrat'}</a></li>
+                            <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/sweet-food.png" alt="" /><a className="link-category" href="#sweets">{window.location.pathname === '/en' ? 'Sweets' : 'Embelsira'}</a></li>
+                            <li className="caategory-li" onClick={() => closeCategory()}><img width="50" src="/water-bottle.png" alt="" /><a className="link-category" href="#other">{window.location.pathname === '/en' ? 'Other drinks' : 'Pije te tjera'}</a></li>
+                        </div>
                     </ul>
                 </nav>
-                <hr className="hr"/>
+                <hr className="hr" />
                 <div className="div-social">
-                            <h1>{window.location.pathname === '/en' ? 'Follow us on' : 'Na ndiqni '}</h1>
-                            <div style={{width:'20%',height:'10%',display:'flex',justifyContent:'space-between',alignItems:'center'}}>
-                                <SiFacebook  style={{color:'white',fontSize:'30px'}} />
-                                <SiInstagram style={{color:'white',fontSize:'30px'}} />
-                                <SiTripadvisor style={{color:'white',fontSize:'30px'}} />                                
-                            </div>
+                    <h1>{window.location.pathname === '/en' ? 'Follow us on' : 'Na ndiqni '}</h1>
+                    <div className="links-social-media-div">
+                        <a target="_blank" href="https://www.facebook.com/greenandproteintr/"><SiFacebook style={{ color: 'white', fontSize: '40px' }} /></a>
+                        <a target="_blank" href="https://www.instagram.com/greenandproteintirana/"> <SiInstagram style={{ color: 'white', fontSize: '40px' }} /></a>
+                        <a target="_blank" href="https://www.tripadvisor.com/Restaurant_Review-g294446-d19240567-Reviews-Green_Protein-Tirana_Tirana_County.html"> <SiTripadvisor style={{ color: 'white', fontSize: '40px' }} /></a>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/LangaugeOption.jsx
+++ src/components/LangaugeOption.jsx
@@ -5,18 +5,20 @@ export default function LangaugeOption() {
         <div className="langauge-choose">
             <div className="language-opa"></div>
             <div className="cotainer-language">
-                <h1 style={{ textAlign: 'center', color: 'white' }}>Zgjidh gjuhen / Choose a language</h1>
+                <img src="/black-logo.png" alt=""/>
+                <div className="choose-language-title">
+                <h1 className="h1-choose-language"> <span> Zgjidh gjuhen /</span> <span> Choose a language</span></h1>
+                </div>
                 <div>
                     <Link onClick={() => {
                         localStorage.setItem('lan', JSON.stringify(true))
                         localStorage.setItem('chosen', JSON.stringify('al'))
-                    }} style={{ fontSize: '30px', textDecoration: 'none', color: 'white' }} to="/">Albania</Link>
-                    <span style={{ fontSize: '30px', cursor: 'pointer', color: 'white' }}> / </span>
+                    }} style={{ fontSize: '30px', textDecoration: 'none', color: 'white' }} to="/"> <img width="80" src="/al.png" alt=""/> </Link>
                     <Link onClick={() => {
                         localStorage.setItem('lan',  JSON.stringify(true))
                         localStorage.setItem('chosen', JSON.stringify('en'))
                     }}
-                        style={{ fontSize: '30px', textDecoration: 'none', color: 'white' }} to="/en">English</Link>
+                        style={{ fontSize: '30px', textDecoration: 'none', color: 'white' }} to="/en"><img width="80" src="/en.png" alt=""/></Link>
                 </div>
             </div>
         </div>
